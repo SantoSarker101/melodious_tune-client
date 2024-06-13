01aee0772abaea059dca09d93778dec13b63d90d
SantoSarker101 <santosarker404@gmail.com>
2024-06-14 02:44:09 +0600

    get classes data in Classes Page

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -140,8 +140,8 @@ const Sidebar = () => {
 
       : <>
 
-<NavLink
-        to='/dashboard/bookings'
+    <NavLink
+        to='/dashboard/selected-classes'
         className={({ isActive }) =>
           `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform       hover:bg-gray-300   hover:text-gray-700 ${
            isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
@@ -151,7 +151,22 @@ const Sidebar = () => {
 
         <FaBuffer className='w-5 h-5' />
 
-        <span className='mx-4 font-medium'>Bookings</span>
+        <span className='mx-4 font-medium'>My Selected Classes</span>
+      </NavLink>
+
+
+    <NavLink
+        to='/dashboard/enrolled-classes'
+        className={({ isActive }) =>
+          `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform       hover:bg-gray-300   hover:text-gray-700 ${
+           isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
+          }`
+        }
+      >
+
+        <FaBuffer className='w-5 h-5' />
+
+        <span className='mx-4 font-medium'>My Enrolled Classes</span>
       </NavLink>
 
       </>
--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -2,22 +2,76 @@ import { useContext, useEffect, useState } from "react";
 import useClasses from "../../Hooks/useClasses";
 import { AuthContext } from "../../providers/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const Classes = () => {
 	const [approvedClasses, setApprovedClasses] = useState([])
 	const [classes] = useClasses()
 	const {user} = useContext(AuthContext)
 	const navigate = useNavigate()
+	const [axiosSecure] = useAxiosSecure()
 
-	// useEffect(() => {
-	// 	if(classes && classes.length > 0){
-	// 		const appropedClasses =
+	useEffect(() => {
+		if(classes && classes.length > 0){
+			const approvedClasses = classes.filter(cl => cl.status === 'Approved')
+			// console.log(approvedClasses);
+			setApprovedClasses(approvedClasses)
+		}
+	},[classes])
+
+
+
+	// const handleSelectedClasses = cls => {
+	// 	console.log(user);
+	// 	const student = {
+	// 		studentName: user.displayName,
+	// 		studentPhoto: user.photoURL,
+	// 		studentEmail: user.email
 	// 	}
-	// },[])
+	// 	const {_id, name, image, seats, price, category, status, details, enrolled, instructorInfo} = cls;
+
+	// 	const selectedClass = {classId: _id, name, image, seats, price, category, status, details, enrolled, instructorInfo, studentInfo: student}
+
+	// 	if(user){
+	// 		axiosSecure.post('/selectedClass', selectedClass)
+	// 		.then(res => {
+	// 			console.log(res.data);
+
+
+	// 			// if (res.data.insertedId) {
+    //             //     Swal.fire({
+    //             //         position: 'top-end',
+    //             //         icon: 'success',
+    //             //         title: 'class Selected successfully',
+    //             //         showConfirmButton: false,
+    //             //         timer: 1500
+    //             //     })
+    //             // }
+	// 		})
+	// 	}
+	// 	// Swal.fire({
+	// 	// 	title: 'Please Login to select the Class',
+	// 	// 	icon: 'warning',
+	// 	// 	showCancelButton: true,
+	// 	// 	confirmButtonColor: '#3085d6',
+	// 	// 	cancelButtonColor: '#d33',
+	// 	// 	confirmButtonText: 'Login'
+	// 	// }).then((result) => {
+	// 	// 	if (result.isConfirmed) {
+	// 	// 	navigate('/login')
+	// 	// 	}
+
+	// 	// })
+	// }
+
 
 	return (
 		<div>
-			<h1>All Approved Classes</h1>
+			<h1>All Classes</h1>
+
+
+
 
 
 		</div>
