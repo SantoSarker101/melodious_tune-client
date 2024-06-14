3a1cf27bea8eccab7a17d26843a612d57bfb35f9
SantoSarker101 <santosarker404@gmail.com>
2024-06-14 23:08:44 +0600

    Creating My Selected Classes Page and recently used reduce for implement price

--- src/pages/Dashboard/SelectedClasses/SelectedClasses.jsx
+++ src/pages/Dashboard/SelectedClasses/SelectedClasses.jsx
@@ -2,9 +2,19 @@ import useSelectedClasses from "../../../Hooks/useSelectedClasses";
 
 const SelectedClasses = () => {
 	const [selectedClasses] = useSelectedClasses()
+
+	const total = selectedClasses.reduce((sum, classItem) => classItem.price + sum, 0)
 	return (
 		<div>
+			<div className="uppercase">
+
 			<h3 className="text-3xl font-extrabold text-white text-center">Selected Classes: {selectedClasses.length}</h3>
+
+			<h3 className="text-3xl font-extrabold text-white text-center">Total Price: $ {total}</h3>
+
+			</div>
+
+
 		</div>
 	);
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -4,11 +4,14 @@ import { FaShoppingCart } from "react-icons/fa";
 import useSelectedClasses from "../../../Hooks/useSelectedClasses";
 import useAdmin from "../../../Hooks/useAdmin";
 import useInstructor from "../../../Hooks/useInstructor";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
 	const [selectedClasses] = useSelectedClasses()
 	const [isAdmin] = useAdmin()
 	const [isInstructor] = useInstructor()
+	const {user} = useContext(AuthContext)
 	return (
 		<div className="px-4 fixed w-full bg-white z-10 bg-opacity-10 shadow-sm">
 			<div className="py-4 border-b-1">
@@ -16,14 +19,18 @@ const NavBar = () => {
 			<div className="flex flexrow items-center justify-between gap-3 md:gap-0">
 				<Link to='/' className="text-lg md:text-3xl text-purple-600 font-extrabold">Melodious Tune</Link>
 
+			{
+				user && <div>
 				{
-					isAdmin || isInstructor || <div>
-					<Link to='/dashboard/selected-classes'>
-					<FaShoppingCart></FaShoppingCart>
-					<div className="badge badge-secondary">+{selectedClasses.length || 0}</div>
-					</Link>
-				</div>
-				}
+				isAdmin || isInstructor || <div>
+				<Link to='/dashboard/selected-classes'>
+				<FaShoppingCart></FaShoppingCart>
+				<div className="badge badge-secondary">+{selectedClasses.length || 0}</div>
+				</Link>
+			</div>
+			}
+			</div>
+			}
 
 				<DropdownMenu></DropdownMenu>
 			</div>
